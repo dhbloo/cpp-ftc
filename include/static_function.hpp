674e85261c066c1ab23c7210a0a17bd61c2fb2f2
dhb <1084714805@qq.com>
2020-01-13 13:56:16 +0800

    update: static_function
    update: static_function
    
    update: static_function
    
    update: static_function
    
    update

--- include/static_function.hpp
+++ include/static_function.hpp
@@ -1,52 +1,177 @@
-#include <cassert>
-#include <memory>
+#pragma once
+#include <functional>  // std::bad_function_call
+#include <utility>     // std::forward, std::move
 
-template <typename> class static_function; /* undefined */
+template <typename, std::size_t BufferSize = 24> class static_function; /* undefined */
 
-template <typename Result, typename... Args> class static_function<Result(Args...)>
+template <typename T> struct __is_static_function
 {
-public:
-    static_function() = default;
-    template <typename T> static_function(T &&t) { operator=(std::forward<T &&>(t)); }
+    static constexpr bool value = false;
+};
 
-    template <typename T> static_function &operator=(T t)
+template <typename Result, typename... Args, std::size_t BufferSize>
+struct __is_static_function<static_function<Result(Args...), BufferSize>>
+{
+    static constexpr bool value = true;
+};
+
+template <typename Result, typename... Args, std::size_t BufferSize>
+class static_function<Result(Args...), BufferSize>
+{
+    static_assert(BufferSize >= 8, "buffer size should be at least 8");
+
+public:
+    static_function() noexcept { new (storage) CallableT<std::nullptr_t>(nullptr); }
+    ~static_function() { ((ICallable *)storage)->~ICallable(); }
+
+    static_function(const static_function &other) noexcept
     {
-        static_assert(sizeof(CallableT<T>) <= sizeof(buffer), "function is too large for buffer");
-        callable.reset(new (buffer) CallableT<T>(t));
+        const ICallable *_callable = reinterpret_cast<const ICallable *>(other.storage);
+        _callable->Clone((void *)storage);
+    }
+
+    static_function(static_function &&other) noexcept
+    {
+        ICallable *_callable = reinterpret_cast<ICallable *>(other.storage);
+        _callable->Move((void *)storage);
+        other = nullptr;
+    }
+
+    template <std::size_t BufferSizeT>
+    static_function(const static_function<Result(Args...), BufferSizeT> &t) noexcept
+    {
+        static_assert(BufferSizeT <= BufferSize, "buffer size is smaller than needed");
+        const ICallable *_callable = reinterpret_cast<const ICallable *>(
+            reinterpret_cast<const static_function &>(t).storage);
+        _callable->Clone((void *)storage);
+    }
+
+    template <std::size_t BufferSizeT>
+    static_function(static_function<Result(Args...), BufferSizeT> &&t) noexcept
+    {
+        static_assert(BufferSizeT <= BufferSize, "buffer size is smaller than needed");
+        static_function &other     = reinterpret_cast<static_function &>(t);
+        ICallable *      _callable = reinterpret_cast<ICallable *>(other.storage);
+        _callable->Move((void *)storage);
+        other = nullptr;
+    }
+
+    template <typename T,
+              typename = typename std::enable_if_t<
+                  !std::is_function<T>::value
+                  && !__is_static_function<typename std::decay<T>::type>::value>>
+    static_function(T &&t) noexcept
+    {
+        using CallableType = typename std::decay<T>::type;
+        static_assert(sizeof(CallableT<CallableType>) <= sizeof(storage),
+                      "function is too large for buffer");
+        new (storage) CallableT<CallableType>(std::forward<T>(t));
+    }
+
+    template <typename T,
+              typename = typename std::enable_if_t<
+                  !std::is_function<T>::value
+                  && !__is_static_function<typename std::decay<T>::type>::value>>
+    static_function &operator=(T &&t) noexcept
+    {
+        using CallableType = typename std::decay<T>::type;
+        static_assert(sizeof(CallableT<CallableType>) <= sizeof(storage),
+                      "function is too large for buffer");
+        ((ICallable *)storage)->~ICallable();
+        new (storage) CallableT<CallableType>(std::forward<T>(t));
         return *this;
     }
 
-    Result operator()(Args... args) const
+    static_function &operator=(const static_function &other) noexcept
     {
-        assert(callable);
-        return callable->Invoke(args...);
+        if (this == &other)
+            return *this;
+        ((ICallable *)storage)->~ICallable();
+        const ICallable *_callable = reinterpret_cast<const ICallable *>(other.storage);
+        _callable->Clone((void *)storage);
+        return *this;
     }
+
+    static_function &operator=(static_function &&other) noexcept
+    {
+        if (this == &other)
+            return *this;
+        ((ICallable *)storage)->~ICallable();
+        ICallable *_callable = reinterpret_cast<ICallable *>(other.storage);
+        _callable->Move((void *)storage);
+        other = nullptr;
+        return *this;
+    }
+
+    template <std::size_t BufferSizeT>
+    static_function &operator=(const static_function<Result(Args...), BufferSizeT> &t) noexcept
+    {
+        static_assert(BufferSizeT <= BufferSize, "buffer size is smaller than needed");
+        const static_function &other = reinterpret_cast<const static_function &>(t);
+        ((ICallable *)storage)->~ICallable();
+        const ICallable *_callable = reinterpret_cast<const ICallable *>(other.storage);
+        _callable->Clone((void *)storage);
+        return *this;
+    }
+
+    template <std::size_t BufferSizeT>
+    static_function &operator=(static_function<Result(Args...), BufferSizeT> &&t) noexcept
+    {
+        static_assert(BufferSizeT <= BufferSize, "buffer size is smaller than needed");
+        static_function &other = reinterpret_cast<static_function &>(t);
+        ((ICallable *)storage)->~ICallable();
+        ICallable *_callable = reinterpret_cast<ICallable *>(other.storage);
+        _callable->Move((void *)storage);
+        other = nullptr;
+        return *this;
+    }
+
+    explicit operator bool() const noexcept { return ((ICallable *)storage)->IsNotEmpty(); }
+    Result   operator()(Args... args) const { return ((ICallable *)storage)->Invoke(args...); }
+
+    friend bool operator==(const static_function &f, std::nullptr_t p) noexcept { return !(bool)f; }
+    friend bool operator==(std::nullptr_t p, const static_function &f) noexcept { return !(bool)f; }
+    friend bool operator!=(const static_function &f, std::nullptr_t p) noexcept { return (bool)f; }
+    friend bool operator!=(std::nullptr_t p, const static_function &f) noexcept { return (bool)f; }
 
 private:
     class ICallable
     {
     public:
-        virtual ~ICallable()                = default;
-        virtual Result Invoke(Args... args) = 0;
+        virtual ~ICallable() noexcept                         = default;
+        virtual bool   IsNotEmpty() const noexcept            = 0;
+        virtual Result Invoke(Args... args)                   = 0;
+        virtual void   Clone(void *dstStorage) const noexcept = 0;
+        virtual void   Move(void *dstStorage) noexcept        = 0;
     };
 
-    template <typename T> class CallableT : public ICallable
+    template <typename T, typename Dummy = void> class CallableT : public ICallable
     {
     public:
-        CallableT(const T &t) : t(t) {}
-        ~CallableT() override = default;
+        CallableT(const T &callable) : f(callable) {}
+        CallableT(T &&callable) : f(std::move(callable)) {}
+        ~CallableT() noexcept override = default;
 
-        Result Invoke(Args... args) override { return t(args...); }
+        bool   IsNotEmpty() const noexcept override { return true; }
+        Result Invoke(Args... args) override { return f(args...); }
+        void   Clone(void *dst) const noexcept override { new (dst) CallableT(f); }
+        void   Move(void *dst) noexcept override { new (dst) CallableT(std::move(f)); }
 
     private:
-        T t;
+        T f;
     };
 
-    struct Deleter
+    template <typename Dummy> class CallableT<std::nullptr_t, Dummy> : public ICallable
     {
-        void operator()(ICallable *callable) { callable->~ICallable(); }
+    public:
+        CallableT(const std::nullptr_t &t) {}
+        ~CallableT() noexcept override = default;
+
+        bool   IsNotEmpty() const noexcept override { return false; }
+        Result Invoke(Args... args) override { throw std::bad_function_call(); }
+        void   Clone(void *dst) const noexcept override { new (dst) CallableT(nullptr); }
+        void   Move(void *dst) noexcept override { new (dst) CallableT(nullptr); }
     };
 
-    char                                buffer[16];
-    std::unique_ptr<ICallable, Deleter> callable;  // used 8 bytes
+    char storage[BufferSize + 8];
 };
--- test/static_function.cpp
+++ test/static_function.cpp
@@ -1,28 +1,163 @@
 #include "static_function.hpp"
 
-#include <iostream>
+#include <gtest/gtest.h>
 
-void func()
+TEST(StaticFunction, Size)
 {
-    std::cout << "func" << std::endl;
+    EXPECT_EQ(sizeof(static_function<void()>), 32);
+    EXPECT_EQ(sizeof(static_function<void(), 8>), 16);
+    EXPECT_EQ(sizeof(static_function<void(), 16>), 24);
+    EXPECT_EQ(sizeof(static_function<void(), 24>), 32);
+    EXPECT_EQ(sizeof(static_function<void(), 32>), 40);
+    EXPECT_EQ(sizeof(static_function<void(), 40>), 48);
+    EXPECT_EQ(sizeof(static_function<void(), 48>), 56);
+    EXPECT_EQ(sizeof(static_function<void(), 56>), 64);
 }
 
-struct functor
+int func(int x)
 {
-    void operator()() { std::cout << "functor" << std::endl; }
-};
+    return x + 42;
+}
 
-int main()
+TEST(StaticFunction, FunctionPointer)
 {
-    std::cout << "sizeof static_function<void()>: " << sizeof(static_function<void()>) << std::endl;
-    static_function<void()> f(func);
-    f();
-    f = functor();
-    f();
-    f = [] { std::cout << "lambda" << std::endl; };
-    f();
+    static_function<int(int)>     sf1(func);
+    static_function<int(int), 8>  sf2(func);
+    static_function<int(int), 16> sf3(func);
+    static_function<int(int), 32> sf4(func);
 
-    char array[8] = "123";
-    f             = [array] { std::cout << array << std::endl; };
-    f();
+    static_function<int(int)>     sf5 = func;
+    static_function<int(int), 8>  sf6 = func;
+    static_function<int(int), 16> sf7 = func;
+    static_function<int(int), 32> sf8 = func;
+    for (int i = 0; i < 10; i++) {
+        EXPECT_EQ(sf1(i), i + 42);
+        EXPECT_EQ(sf2(i), i + 42);
+        EXPECT_EQ(sf3(i), i + 42);
+        EXPECT_EQ(sf4(i), i + 42);
+        EXPECT_EQ(sf5(i), i + 42);
+        EXPECT_EQ(sf6(i), i + 42);
+        EXPECT_EQ(sf7(i), i + 42);
+        EXPECT_EQ(sf8(i), i + 42);
+    }
+}
+
+TEST(StaticFunction, Functor)
+{
+    struct functor
+    {
+        int operator()(int x) { return x + 42; }
+    } f;
+
+    static_function<int(int)>     sf1(f);
+    static_function<int(int), 8>  sf2(f);
+    static_function<int(int), 16> sf3(f);
+    static_function<int(int), 32> sf4(f);
+
+    static_function<int(int)>     sf5 = f;
+    static_function<int(int), 8>  sf6 = f;
+    static_function<int(int), 16> sf7 = f;
+    static_function<int(int), 32> sf8 = f;
+    for (int i = 0; i < 10; i++) {
+        EXPECT_EQ(sf1(i), i + 42);
+        EXPECT_EQ(sf2(i), i + 42);
+        EXPECT_EQ(sf3(i), i + 42);
+        EXPECT_EQ(sf4(i), i + 42);
+        EXPECT_EQ(sf5(i), i + 42);
+        EXPECT_EQ(sf6(i), i + 42);
+        EXPECT_EQ(sf7(i), i + 42);
+        EXPECT_EQ(sf8(i), i + 42);
+    }
+}
+
+TEST(StaticFunction, Lambda)
+{
+    int  y = 42;
+    auto f = [y](int x) { return x + y; };
+
+    static_function<int(int)>     sf1(f);
+    static_function<int(int), 8>  sf2(f);
+    static_function<int(int), 16> sf3(f);
+    static_function<int(int), 32> sf4(f);
+
+    static_function<int(int)>     sf5 = f;
+    static_function<int(int), 8>  sf6 = f;
+    static_function<int(int), 16> sf7 = f;
+    static_function<int(int), 32> sf8 = f;
+    for (int i = 0; i < 10; i++) {
+        EXPECT_EQ(sf1(i), i + 42);
+        EXPECT_EQ(sf2(i), i + 42);
+        EXPECT_EQ(sf3(i), i + 42);
+        EXPECT_EQ(sf4(i), i + 42);
+        EXPECT_EQ(sf5(i), i + 42);
+        EXPECT_EQ(sf6(i), i + 42);
+        EXPECT_EQ(sf7(i), i + 42);
+        EXPECT_EQ(sf8(i), i + 42);
+    }
+}
+
+TEST(StaticFunction, Assign)
+{
+    int  y = 42;
+    auto f = [y](int x) { return x + y; };
+
+    static_function<int(int)> sf1(f);
+    static_function<int(int)> sf2(sf1);
+    static_function<int(int)> sf3(sf2);
+    static_function<int(int)> sf4 = sf3;
+    static_function<int(int)> sf5 = sf4;
+    static_function<int(int)> sf6;
+    sf6 = sf5;
+
+    for (int i = 0; i < 10; i++) {
+        EXPECT_EQ(sf1(i), sf2(i));
+        EXPECT_EQ(sf1(i), sf3(i));
+        EXPECT_EQ(sf1(i), sf4(i));
+        EXPECT_EQ(sf1(i), sf5(i));
+        EXPECT_EQ(sf1(i), sf6(i));
+    }
+}
+
+TEST(StaticFunction, OperatorBool)
+{
+    int  y = 42;
+    auto f = [y](int x) { return x + y; };
+
+    static_function<int(int)> sf1(f);
+    static_function<int(int)> sf2;
+    static_function<int(int)> sf3 = sf1;
+
+    EXPECT_EQ((bool)sf1, true);
+    EXPECT_EQ((bool)sf2, false);
+    EXPECT_EQ((bool)sf3, true);
+
+    sf3 = nullptr;
+    EXPECT_EQ((bool)sf3, false);
+}
+
+TEST(StaticFunction, OperatorEqual)
+{
+    int  y = 42;
+    auto f = [y](int x) { return x + y; };
+
+    static_function<int(int)> sf1(f);
+    static_function<int(int)> sf2;
+    static_function<int(int)> sf3 = sf1;
+
+    EXPECT_EQ(sf1 == nullptr, false);
+    EXPECT_EQ(sf1 != nullptr, true);
+    EXPECT_EQ(sf2 == nullptr, true);
+    EXPECT_EQ(sf2 != nullptr, false);
+    EXPECT_EQ(sf3 == nullptr, false);
+    EXPECT_EQ(sf3 != nullptr, true);
+
+    sf3 = nullptr;
+    EXPECT_EQ(sf3 == nullptr, true);
+    EXPECT_EQ(sf3 != nullptr, false);
+}
+
+int main(int argc, char *argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
