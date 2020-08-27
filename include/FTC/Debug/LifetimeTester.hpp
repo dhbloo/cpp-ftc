f335c059058a798047388fecc3b7d3a2ba552824
DHB <duanhb1084@qq.com>
2020-08-27 09:45:47 +0800

    update: add noexcept for lifetimeTester

--- include/FTC/Debug/LifetimeTester.hpp
+++ include/FTC/Debug/LifetimeTester.hpp
@@ -25,29 +25,29 @@ template <typename OutputWarpper = StdoutWrapper, std::size_t NameSize = 16>
 class LifetimeTester
 {
 public:
-    LifetimeTester(const char *_name = "Unnamed")
+    LifetimeTester(const char *_name = "Unnamed") noexcept
     {
         strncpy(name, _name, sizeof(name));
         OutputWarpper() << name << ": Default constructor called.\n";
     }
 
-    LifetimeTester(const LifetimeTester &t)
+    LifetimeTester(const LifetimeTester &t) noexcept
     {
         strncpy(name, t.name, sizeof(name));
         OutputWarpper() << name << ": Copy constructor called.\n";
     }
-    LifetimeTester(LifetimeTester &&t)
+    LifetimeTester(LifetimeTester &&t) noexcept
     {
         strncpy(name, t.name, sizeof(name));
         OutputWarpper() << name << ": Move constructor called.\n";
     }
 
-    LifetimeTester &operator=(const LifetimeTester &t)
+    LifetimeTester &operator=(const LifetimeTester &t) noexcept
     {
         strncpy(name, t.name, sizeof(name));
         OutputWarpper() << name << ": Copy Assignment called.\n";
     }
-    LifetimeTester &operator=(LifetimeTester &&t)
+    LifetimeTester &operator=(LifetimeTester &&t) noexcept
     {
         strncpy(name, t.name, sizeof(name));
         OutputWarpper() << name << ": Move Assignment called.\n";
